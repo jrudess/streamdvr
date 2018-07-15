f0d717de987a4a3f7bc2532841d5ece45704072e
jrudess <jrudess>
2018-07-15 10:50:16 -0700

    Fix #56, change cam4 to use best/rtmp streams

--- plugins/cam4.js
+++ plugins/cam4.js
@@ -2,7 +2,7 @@ const streamlink = require("../core/streamlink");
 
 class Cam4 extends streamlink.Streamlink {
     constructor(tui) {
-        super("CAM4", tui, "https://www.cam4.com/", false, "480p");
+        super("CAM4", tui, "https://www.cam4.com/", true, "best");
     }
 }
 
