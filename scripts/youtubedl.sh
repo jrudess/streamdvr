abe564b4b2800c8d6a9fcb3e1c1d9fc2ce7e1056
jrudess <jrudess>
2018-11-23 12:15:47 -0800

    Add youtube-dl m3u8 lookup script

---
+++ scripts/youtubedl.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/bash
+# Return 0 if streamer is online and m3u8 in stdout
+# Return 1 if streamer is offline and print unexpected errors to stdout
+
+tmp=$(mktemp -d)
+trap 'rm -rf "$tmp"' EXIT
+
+args=("$@")
+url=${args[0]}
+proxyen=${args[1]}
+proxyserver=""
+if [ "$proxyen" -eq 1 ]; then
+    $proxyserver="--proxyserver ${args[2]}"
+fi
+
+youtube-dl -g $url $proxyserver > $tmp/stdout 2> $tmp/stderr
+
+if [ "$?" -eq 0 ]; then
+    # Streamer is online
+    cat $tmp/stdout
+    exit 0
+else
+    grep -i "offline" $tmp/stout > /dev/null 2> /dev/null
+    if [ "$?" -ne 0 ]; then
+        # Print nothing for offline cases, these are expected errors
+        exit 1
+    fi
+
+    # Print the error message to stdout for streamdvr to reprint
+    cat $tmp/stdout
+    exit 1
+fi
