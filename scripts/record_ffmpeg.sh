33873de38d2e8d49acf4f1c6af45a820b71eea57
jrudess <jordan.r.rudess@gmail.com>
2018-12-01 20:02:18 -0800

    Fix ffmpeg debug

--- scripts/record_ffmpeg.sh
+++ scripts/record_ffmpeg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source scripts/record_setup.sh
 
-debugargs=$([ "$debug" = 1 ] && echo "-v fatal" || echo "")
+debugargs=$([ "$debug" = 1 ] && echo "" || echo "-v fatal")
 
 ffmpeg -hide_banner -i $url -c copy -vsync 2 -r 60 -b:v 500k $extraargs $debugargs $output &
 record_pid=$!
